197b35aea1d3f30ad6f93b87563f5c88fcf086c7
timothyAgevi <timothyagevi@gmail.com>
2023-07-30 16:42:23 +0300

    add function to close modal

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ async function connect() {
         document.getElementById("token_modal").style.display = "block";
       }
       
+      function closeModal(){
+        document.getElementById("token_modal").style.display = "none";
+        }
       document.getElementById("login_button").onclick = connect;
       document.getElementById("from_token_select").onclick = openModal;
       document.getElementById("modal_close").onclick = closeModal;
